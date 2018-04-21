c0d87e2cce0b28b692c702c1a2c50d1a1bc9288d
macaroonable <hugo.song33@gmail.com>
2018-04-21 14:11:07 -0700

    add log messaging

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send({ hi: "there does it work" });
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 console.log("port is " + port);
