fa6114bad2ba2f13a6b7bc7cf8212a96072c2176
macaroonable <hugo.song33@gmail.com>
2018-04-21 14:05:23 -0700

    try again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "there does it work" });
 });
 
 app.listen(process.env.port || 5000);
