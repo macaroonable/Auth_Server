50166de77f6c52789a578d6312768ba02c014283
macaroonable <hugo.song33@gmail.com>
2018-04-22 21:39:53 -0700

    modify the path for production

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  callbackURL: "http://dope-stuff.herokuapp.com/auth/google/callback"
+  callbackURL: "https://dope-stuff.herokuapp.com/auth/google/callback"
 };
