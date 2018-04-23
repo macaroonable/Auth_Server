a5962ede1b4d6163c491273d54538fe3500b3552
macaroonable <hugo.song33@gmail.com>
2018-04-22 21:27:43 -0700

    add default redirect

--- routes/authroute.js
+++ routes/authroute.js
@@ -21,4 +21,7 @@ module.exports = app => {
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
+  app.get("/", (req, res) => {
+    res.redirect("/auth/google");
+  });
 };
